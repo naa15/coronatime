bd6f81391eedddca9c34ef82aa11fccd22313c03
naa15 <naltu15@freeuni.edu.ge>
2022-04-11 17:34:53 +0400

    fix: change mail image source

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,8 +2,8 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 
-<img src="{{ asset('/images/mail-photo.svg')}}" class="logo" alt="Coronatime">
-{{-- <img src="https://coronatime.nanuka.redberryinternship.ge/images/mail-photo.svg" class="logo" alt="Coronatime"> --}}
+{{-- <img src="{{ asset('/images/mail-photo.svg')}}" class="logo" alt="Coronatime"> --}}
+<img src="https://coronatime.nanuka.redberryinternship.ge/images/mail-photo.svg" class="logo" alt="Coronatime">
 
 </a>
 </td>
